dc0b38d4f3af9d883506a5537db0986e2548ff57
reneric <rsimmo4@gmail.com>
2019-05-23 13:25:14 -0500

    Update

--- OchsnerFanInsertsTeensy.ino
+++ OchsnerFanInsertsTeensy.ino
@@ -1,3 +1,4 @@
+#define FASTLED_ALLOW_INTERRUPTS 0
 #include <SPI.h>
 #include <Ethernet.h>
 #include <PubSubClient.h>
@@ -5,23 +6,27 @@
 
 FASTLED_USING_NAMESPACE
 
-#define TEST_PIN    7
+// MUST SET CPU SPEED @ 24MHz TO AVOID FLICKER [AND SO AS NOT TO OVERCLOCK]
 
+// #define TEST_PIN    7
 #define DEBUG 1
+// #define LOCAL true
 
 #define DATA_PIN    5
-#define LED_TYPE    WS2811
+#define LED_TYPE    WS2812B
 #define COLOR_ORDER GRB
 #define NUM_LEDS    400
 
-#define BRIGHTNESS          96
-#define FRAMES_PER_SECOND  120
+#define BRIGHTNESS         150
+#define FRAMES_PER_SECOND  96
 
 CRGB leds[NUM_LEDS];
 
 // Update these with values suitable for the hardware/network.
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xEA };
 IPAddress ip(192, 168, 2, 106);
+
+
 // byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
 // IPAddress ip(192, 168, 1, 82);
 
@@ -41,9 +46,9 @@ const int PRESENT_STATE = 1;
 const int ACTIVE_STATE = 2;
 const int COMPLETE_STATE = 3;
 
-int LED_state = 0;
+int LED_state = 1;
 
-// Initialize the current state ON or OFF
+// Initialize the current state of LIGHT EFFECT
 int currentState = IDLE_STATE;
 int tempState;
 
@@ -52,16 +57,22 @@ EthernetClient net;
 // Initialize the MQTT library
 PubSubClient mqttClient(net);
 
-// const char* mqttServer = "192.168.1.97";
-const int mqttPort = 1883;
+#ifdef LOCAL
+const char* mqttServer = "192.168.1.97";
+#else
 const char* mqttServer = "192.168.2.10";
+#endif
+const int mqttPort = 1883;
+
 
 // Station states, used as MQTT Messages
 const char states[4][30] = {"IDLE", "PRESENT", "ACTIVE", "COMPLETE"};
 
+/*********** RECONNECT WITHOUT BLOCKING LOOP ************/
+long lastReconnectAttempt = 0;
 boolean reconnect_non_blocking() {
   if (mqttClient.connect(CLIENT_ID)) {
-      // Serial.println("Connected!");
+       Serial.print("Connected!");
       // Once connected, publish an announcement...
       mqttClient.publish(CLIENT_ID, "CONNECTED", true);
 
@@ -77,17 +88,15 @@ boolean reconnect_non_blocking() {
   return mqttClient.connected();
 }
 
-// List of patterns to cycle through.
+/****************** LIST OF PATTERNS TO CYCLE THRU **************/
 typedef void (*EffectsList[])();
-EffectsList effects = { rainbow, rainbowWithGlitter, confetti, sinelon, juggle, bpm };
+EffectsList effects = { blue_pulse, blue_pulse_split, blue_pulse_white, blue_pulse_split_white, rainbow, confetti, sinelon, juggle, bpm, ochsnerSparkle};
 
 uint8_t currentEffect = 0; // Index number of which pattern is current
 uint8_t gHue = 0; // rotating "base color" used by many of the patterns
 
-long lastReconnectAttempt = 0;
 
-void nextPattern();
-
+/**** CALLBACK RUN WHEN AN MQTT MESSAGE IS RECEIVED *****/
 void messageReceived(char* topic, byte* payload, unsigned int length) {
   Serial.print("Message arrived [");
   Serial.print(topic);  
@@ -131,20 +140,25 @@ void setup() {
   Ethernet.begin(mac, ip);
 //   Ethernet.begin(mac, ip, myDns); // Connect with DNS 
 
-  mqttClient.setServer(mqttServer, mqttPort);
+ 
+
+  mqttClient.setServer(mqttServer, 1883);
   mqttClient.setCallback(messageReceived);
   
-  FastLED.addLeds<LED_TYPE,DATA_PIN,COLOR_ORDER>(leds, NUM_LEDS);
+  FastLED.addLeds<LED_TYPE,DATA_PIN,COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
 
-  pinMode(TEST_PIN, INPUT);
+  // pinMode(TEST_PIN, INPUT);
+
+  FastLED.setDither(0);
   // Set master brightness control
   FastLED.setBrightness(BRIGHTNESS);
   lastReconnectAttempt = 0;
-  LED_state = 0;
 }
 
 int testModeTimer = 0;
+
 void loop() {
+/******************  
   boolean testMode = digitalRead(TEST_PIN) == HIGH;
   if (testMode) {
     if (millis() - testModeTimer > 8000) {
@@ -153,7 +167,8 @@ void loop() {
       if (currentState > 3) currentState = 0;
     }
   }
-  else {
+  else ****/ 
+  { 
     if (!mqttClient.connected()) {
       long now = millis();
       if (now - lastReconnectAttempt > 5000) {
@@ -167,12 +182,17 @@ void loop() {
       mqttClient.loop();
     }
   }
-  Serial.println(LED_state);
+ 
   if (LED_state == 0) {
-      // fill_solid(leds, NUM_LEDS, CRGB::Black);
+      Serial.println("OFF");
+      fill_solid(leds, NUM_LEDS, CRGB::Black);
       FastLED.clear();
   }
   else {
+    
+    Serial.print("Current State: ");
+    Serial.println(currentState);
+
     switch (currentState) {
       case PRESENT_STATE:
         setPresent();
@@ -212,7 +232,7 @@ void stateMachine (int state) {
     Serial.println(state);
 #endif
     currentState = state;
-    mqttClient.publish("chromaFaceStatus", states[currentState], true);
+    mqttClient.publish("FanArrayStatus", states[currentState], true);
   }
 }
 
@@ -221,7 +241,8 @@ void setIdle () {
 #if DEBUG == 1
   Serial.println("Set IDLE");
 #endif
-  fill_solid(leds, NUM_LEDS, CRGB::Black);
+  currentEffect = 0;  // blue pulse
+  effects[currentEffect]();
 }
 
 /********* SET GIVEN STATION STATE TO PRESENT *********/
@@ -229,7 +250,7 @@ void setPresent () {
 #if DEBUG == 1
   Serial.println("Set PRESENT");
 #endif
-  fill_solid(leds, NUM_LEDS, CRGB::White);
+  fill_solid(leds, NUM_LEDS, CRGB::Yellow);
 }
 
 /********** SET GIVEN STATION STATE TO ACTIVE **********/
@@ -237,10 +258,8 @@ void setActive () {
 #if DEBUG == 1  
   Serial.println("ACTIVE");
 #endif
-  static uint8_t startIndex = 0;
-  startIndex = startIndex + 2; /* motion speed */
-    
-  FillLEDsFromPaletteColors(startIndex);
+  currentEffect = 9;  // Ochsner Sparkle (custom)
+  effects[currentEffect]();
 }
 
 /********** SET GIVEN STATION STATE TO COMPLETE **********/
@@ -258,6 +277,8 @@ void nextPattern()
 {
   // add one to the current pattern number, and wrap around at the end
   currentEffect = (currentEffect + 1) % ARRAY_SIZE(effects);
+  Serial.print("CURRENT EFFECT: ");
+  Serial.println(currentEffect);
 }
 
 void testCycle () {
@@ -265,16 +286,45 @@ void testCycle () {
 }
 
 
-/*
- * 
- * LED EFFECTS FUNCTIONS
- *
- *
- */
+/*********** LED EFFECTS FUNCTIONS **********/
+
 void rainbow() 
 {
   // FastLED's built-in rainbow generator
   fill_rainbow( leds, NUM_LEDS, gHue, 7);
+}
+
+int speed = 3;
+void blue_pulse() 
+{
+  static uint8_t startIndex = 0;
+  startIndex = startIndex + 4; /* motion speed */
+
+  FillLEDsFromPaletteColors(startIndex, CRGB::Blue, CRGB::DarkBlue);
+}
+
+void blue_pulse_split() 
+{
+  static uint8_t startIndex = 0;
+  startIndex = startIndex + 4; /* motion speed */
+  
+  FillLEDsFromPaletteColorsSplit(startIndex, CRGB::Blue, CRGB::DarkBlue);
+}
+
+void blue_pulse_white() 
+{
+  static uint8_t startIndex = 0;
+  startIndex = startIndex + 4; /* motion speed */
+  
+  FillLEDsFromPaletteColors(startIndex, CRGB::RoyalBlue, CRGB::LightSkyBlue);
+}
+
+void blue_pulse_split_white() 
+{
+  static uint8_t startIndex = 0;
+  startIndex = startIndex + 6; /* motion speed */
+  
+  FillLEDsFromPaletteColorsSplit(startIndex, CRGB::RoyalBlue, CRGB::LightSkyBlue);
 }
 
 void rainbowWithGlitter() 
@@ -291,12 +341,31 @@ void addGlitter( fract8 chanceOfGlitter)
   }
 }
 
+
+void addGlitterBlue( fract8 chanceOfGlitter) 
+{
+  if( random8() < chanceOfGlitter) {
+    leds[ random16(NUM_LEDS) ] += CRGB::Blue;
+  }
+}
+
+void addGlitterYellow( fract8 chanceOfGlitter) 
+{
+  if( random8() < chanceOfGlitter) {
+    leds[ random16(NUM_LEDS) ] += CRGB::Yellow;
+  }
+}
+
+
 void confetti() 
 {
   // random colored speckles that blink in and fade smoothly
   fadeToBlackBy( leds, NUM_LEDS, 10);
   int pos = random16(NUM_LEDS);
-  leds[pos] += CHSV( gHue + random8(64), 200, 255);
+  int pos2 = random16(NUM_LEDS);
+//  leds[pos] += CHSV( gHue + random8(64), 200, 255);
+  leds[pos] += CHSV( HUE_BLUE, 170 + random8(64), 255);
+  leds[pos2] += CHSV( HUE_YELLOW, 170 + random8(64), 255);
 }
 
 void sinelon()
@@ -328,24 +397,119 @@ void juggle() {
   }
 }
 
+void ochsnerSparkle() {
+  // built specifically for Active state of the AR experience 
+  // confetti x3 x5
+  fadeToBlackBy( leds, NUM_LEDS, 35);
+  int pos = random16(NUM_LEDS);
+  int pos2 = random16(NUM_LEDS);
+  int pos3 = random16(NUM_LEDS);
+  int pos4 = random16(NUM_LEDS);
+  int pos5 = random16(NUM_LEDS);
+  int pos6 = random16(NUM_LEDS);
+  int pos7 = random16(NUM_LEDS);
+  int pos8 = random16(NUM_LEDS);
+  int pos9 = random16(NUM_LEDS);
+  int pos10 = random16(NUM_LEDS);
+  int pos11 = random16(NUM_LEDS);
+  int pos12 = random16(NUM_LEDS);
+  int pos13 = random16(NUM_LEDS);
+  int pos14 = random16(NUM_LEDS);
+  int pos15 = random16(NUM_LEDS);
+  leds[pos] += CHSV( HUE_BLUE, 170 + random8(64), 255);
+  leds[pos2] += CHSV( HUE_YELLOW, 170 + random8(64), 255);
+  leds[pos3] += CHSV( HUE_YELLOW, 1, 255);
+  leds[pos4] += CHSV( HUE_BLUE, 170 + random8(64), 255);
+  leds[pos5] += CHSV( HUE_YELLOW, 170 + random8(64), 255);
+  leds[pos6] += CHSV( HUE_YELLOW, 1, 255);
+  leds[pos7] += CHSV( HUE_BLUE, 170 + random8(64), 255);
+  leds[pos8] += CHSV( HUE_YELLOW, 170 + random8(64), 255);
+  leds[pos9] += CHSV( HUE_YELLOW, 1, 255);
+  leds[pos10] += CHSV( HUE_BLUE, 170 + random8(64), 255);
+  leds[pos11] += CHSV( HUE_YELLOW, 170 + random8(64), 255);
+  leds[pos12] += CHSV( HUE_YELLOW, 1, 255);
+  leds[pos13] += CHSV( HUE_BLUE, 170 + random8(64), 255);
+  leds[pos14] += CHSV( HUE_YELLOW, 170 + random8(64), 255);
+  leds[pos15] += CHSV( HUE_YELLOW, 1, 255);  
 
-void FillLEDsFromPaletteColors( uint8_t colorIndex)
+}
+
+long start = random(NUM_LEDS);
+void FillLEDsFromPaletteColors( uint8_t colorIndex, CRGB blue, CRGB darkBlue)
 {
     for( int i = 0; i < NUM_LEDS; i++) {
-        leds[i] = ColorFromPalette( SetupActivePalette(), colorIndex, BRIGHTNESS, LINEARBLEND);
-        colorIndex += 1;
+        leds[i] = ColorFromPalette(SetupActivePalette(blue, darkBlue), colorIndex, BRIGHTNESS, LINEARBLEND);
+        colorIndex += speed;
     }
 }
 
+void FillLEDsFromPaletteColorsSplit( uint8_t colorIndex, CRGB blue, CRGB darkBlue)
+{
+    for( int i = 0; i < 52; i++) {
+        leds[i] = ColorFromPalette(SetupActivePalette(blue, darkBlue), colorIndex, BRIGHTNESS, LINEARBLEND);
+        colorIndex += speed;
+    }
+    for(int i = 0; i < 53; i++) {
+        leds[i+51] = ColorFromPalette(SetupActivePalette(blue, darkBlue), colorIndex, BRIGHTNESS, LINEARBLEND);
+        colorIndex -= speed;
+    }
+}
+
+void FillLEDsFromPaletteColorsWhite( uint8_t colorIndex, CRGB blue, CRGB darkBlue)
+{
+    for( int i = 0; i < NUM_LEDS; i++) {
+        leds[i] = ColorFromPalette(SetupActivePalette(blue, darkBlue), colorIndex, BRIGHTNESS, LINEARBLEND);
+        colorIndex += speed;
+    }
+}
+
+void FillLEDsFromPaletteColorsSplitWhite( uint8_t colorIndex, CRGB blue, CRGB darkBlue)
+{
+    for( int i = 0; i < 52; i++) {
+        leds[i] = ColorFromPalette(SetupActivePalette(blue, darkBlue), colorIndex, BRIGHTNESS, LINEARBLEND);
+        colorIndex += speed;
+    }
+    for(int i = 0; i < 53; i++) {
+        leds[i+51] = ColorFromPalette(SetupActivePalette(blue, darkBlue), colorIndex, BRIGHTNESS, LINEARBLEND);
+        colorIndex -= speed;
+    }
+}
+
+CRGBPalette16 SetupActivePalette(CRGB blue, CRGB darkBlue)
+{
+  CRGB black  = darkBlue;
+    
+    return CRGBPalette16(
+                                   blue,  blue,  black,  black,
+                                   blue,  blue,  blue,  blue,
+                                   blue,  blue,  black,  black,
+                                   darkBlue, darkBlue, black, black);
+}
+
+/****************** UNUSED PALETTES *********************
 CRGBPalette16 SetupActivePalette()
 {
+  
     CRGB blue = CHSV( HUE_BLUE, 255, 255);
     CRGB darkBlue  = CRGB::DarkBlue;
     CRGB black  = CRGB::DarkBlue;
-    
+ 
     return CRGBPalette16(
                                    blue,  blue,  black,  black,
                                    darkBlue, darkBlue, black,  black,
                                    blue,  blue,  black,  black,
                                    darkBlue, darkBlue, black,  black );
 }
+
+
+CRGBPalette16 SetupActivePalette(CRGB blue, CRGB darkBlue)
+{
+   CRGB black  = darkBlue;
+ 
+   return CRGBPalette16(
+                                   blue,  blue,  black,  black,
+                                   black,  blue,  blue,  blue,
+                                   blue,  blue,  black,  black,
+                                   darkBlue, darkBlue, black, black);
+}
+******/
